c83929e04c4f57822c27ecac675e5cb09bc1e038
wuren1993 <56948999+wuren1993@users.noreply.github.com>
2020-11-12 15:47:17 +0900

    添加圣痕

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
-    $DataFilepath = "ArtifactsData.txt";
+    $DataFilepath = 'ArtifactsData.txt';//圣痕数据
     $ArtifactsData = file($DataFilepath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    
-    $sets =  explode(",",$ArtifactsData[0]);
-    $types =  explode(",",$ArtifactsData[1]);
+    $ArtifactsListPath = 'ArtifactsList.txt';//用户圣痕列表
+    $ArtifactsList = file($ArtifactsListPath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    $sets =  explode(",",$ArtifactsData[0]);//套装名
+    $types =  explode(",",$ArtifactsData[1]);//部位类型
+
+    AddToList($ArtifactsListPath);
 
 ?>
 
@@ -20,12 +24,14 @@
 <a href="http://www.baidu.com">百度</a> 
 
 <form method="POST" action="">
-<table>
+    <table>
 
     <tr>
         <th>Rarity</th><th>Lv</th><th>Set</th><th>Type</th><th>Name</th><th>Main</th><th>Sub1</th><th>Sub2</th><th>Sub3</th><th>Sub4</th>
     </tr>
-
+    <?php
+        ShowAritfacts($ArtifactsListPath);
+    ?>
     <tr>
         <td>
             <select name="rarity">
@@ -35,12 +41,11 @@
         </td>
         <td>
             <select name="level">
-                <option value ="0">0</option>
-                <option value ="4">4</option>
-                <option value="8">8</option>
-                <option value="12">12</option>
-                <option value="16">16</option>
-                <option value="20">20</option>
+                <?php
+                    for($i =0;$i<=20;$i++){
+                        echo "<option value =\"".$i."\">".$i."</option>";
+                    }
+                ?>
             </select>
         </td>
         <td>
@@ -61,19 +66,82 @@
             ?>
             </select>
         </td>
-        <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        <?php
+            StatusSelector();
+        ?>        
     </tr>
 
-</table>
-            </form>
-<br>
+    </table>
+    <br><button type="submit">添加</button>
+</form>
+
 
 <script language="javascript">
-    function Refresh()
-    {
-        this.form.submit(); 
+    function Refresh(){
+        this.form.submit();
     }
+    function AddArtifact(){
+        $newArtifact[9];
+
+        this.form.submit();
+    }
+
 </script>
 
 </body>                              
-</html>   
+</html>
+
+<?php
+    function ShowAritfacts($filepss){        
+        $List = file($filepss, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        if($List==null)return;
+        
+        for($i =0;$i<count($List);$i++){
+            $atf =  explode(",",$List[$i]);
+            echo "<tr>";
+            for($j =0;$j<count($atf);$j++){
+                echo "<td>";
+                echo $atf[$j];
+                echo "</td>";
+            }
+            echo "<tr><br>";
+        }
+    }
+
+    function AddToList($filepass){
+        if($_POST["rarity"]==null||$_POST["level"]==null||$_POST["set"]==null||$_POST["type"]==null)
+        return;
+
+        $str =$_POST["rarity"].",".$_POST["level"].",".$_POST["set"].",".$_POST["type"].",".$_POST["main"].",".$_POST["mains"]."\n";
+        file_put_contents($filepass, $str,FILE_APPEND|LOCK_EX);
+    }
+
+    function StatusSelector(){
+        $statusType =["main","sub1","sub2","sub3","sub4","sub5"];
+        
+        foreach($statusType as $str){
+            echo "<td>";
+            StatusOption($str);
+            echo "</td>";
+        }
+    }
+
+    function StatusOption($str){
+        echo "<select name=\"".$str."\">";
+        echo     "<option value =\"atk\">攻击力</option>";
+        echo     "<option value =\"atkp\">攻击力%</option>";
+        echo     "<option value =\"hp\">生命值</option>";
+        echo     "<option value =\"hpp\">生命值%</option>";
+        echo     "<option value =\"def\">防御力</option>";
+        echo     "<option value =\"defp\">防御力%</option>";
+        echo     "<option value =\"erc\">元素充能</option>";
+        echo     "<option value =\"emst\">元素精通</option>";
+        echo     "<option value =\"crit\">暴击</option>";
+        echo     "<option value =\"critd\">爆伤</option>";
+        echo     "<option value =\"ed\">属性伤害</option>";
+        echo     "<option value =\"phd\">物理伤害</option>";
+        echo     "<option value =\"hb\">治疗加成</option>";              
+        echo "</select>";
+        echo "<input type=\"tel\" step=\"0.1\" name=\"".$str."s\" style=\"width:40px;\">";
+    }
+?>
